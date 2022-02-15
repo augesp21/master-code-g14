558300550480a8317cddeb2b53fa52cddd89abd0
augesp21 <augesp21@gmail.com>
2022-02-15 15:18:42 -0600

    nuevo avance de login

--- 02-introAJS/Cajero/js/index.js
+++ 02-introAJS/Cajero/js/index.js
@@ -4,8 +4,62 @@ var cuentas = [
   { nombre: "Maui", saldo: 67, password: "123" },
 ];
 
-
 function login() {
+  let aux = 0;
+  let usuario = document.getElementById("floatingInput").value;
+  let contraseña = document.getElementById("floatingPassword").value;
+
+  for (let i = 0; i < cuentas.length; i++) {
+    if (usuario === cuentas[i].nombre) {
+      aux = 1;
+      if (contraseña === cuentas[i].password) {
+        location = "main.html";
+        aux = 2;
+      }
+    }
+  }
+  if (aux == 2) {
+  } else if (aux == 1 && contraseña === "") {
+    alert("Ingrese contraseña");
+  } else aux == 1 && contraseña != cuentas[i].password;
+  {
+    alert("Contraseña incorrecta");
+  }
+  if (aux == 0 && usuario === "") {
+    alert("Ingrese Usuario");
+  } else aux == 0 && usuario != cuentas[i].nombre;
+  alert("Usuario invalido");
+}
+
+/* function login() {
+  let aux = 0;
+  let usuario = document.getElementById("floatingInput").value;
+  let contraseña = document.getElementById("floatingPassword").value;
+
+  for (let i = 0; i < cuentas.length; i++) {
+    if (usuario === cuentas[i].nombre) {
+      aux = 1;
+      if ((aux = 1 && contraseña === cuentas[i].password)) {
+        location = "main.html";
+        aux = 2;
+      }
+    }
+  }
+  if (aux=2){
+  }
+  else if (aux=1 && contraseña === "") {
+    alert("Ingrese contraseña");
+  } else(aux = 1 && contraseña != cuentas[i].password){
+    alert("Contraseña incorrecta");
+  }
+  if(aux=0 && usuario === "") {
+    alert("Ingrese Usuario");
+  } else (aux=0 && usuario != cuentas[i].nombre){
+    alert("Usuario invalido");
+  }
+} */
+
+/* function login() {
   let usuario = document.getElementById("floatingInput").value;
   let contraseña = document.getElementById("floatingPassword").value;
 
@@ -22,17 +76,17 @@ function login() {
   } else {
     alert("Usuario invalido");
   }
-}
+} */
 
-function saldo(form){
-    location="saldo.html"
+function saldo(form) {
+  location = "saldo.html";
 }
-function retiro(form){
-    location="retiro.html"
+function retiro(form) {
+  location = "retiro.html";
 }
-function ingreso(form){
-    location="ingreso.html"
+function ingreso(form) {
+  location = "ingreso.html";
 }
-function cerrar(form){
-    location="index.html"
+function cerrar(form) {
+  location = "index.html";
 }
